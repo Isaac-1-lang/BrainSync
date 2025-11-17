478a99b659e31f8712a484442bd1029fe105ba8c
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2025-11-17 14:14:38 +0200

    Initial commit!

---
+++ src/main/java/com/brainsync/brainsync/BrainSyncApplication.java
@@ -0,0 +1,13 @@
+package com.brainsync.brainsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brainsync/brainsync/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package com.brainsync.brainsync;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.cassandra.CassandraContainer;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    CassandraContainer cassandraContainer() {
+        return new CassandraContainer(DockerImageName.parse("cassandra:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    OllamaContainer ollamaContainer() {
+        return new OllamaContainer(DockerImageName.parse("ollama/ollama:latest"));
+    }
+
+}
